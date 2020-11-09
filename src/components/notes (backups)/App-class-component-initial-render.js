1a4ec1e4584c943fe528e6084756e978a3e9d454
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-11-09 13:39:57 -0500

    Took some notes,  added first snapshot, about to split App into class & functional components

---
+++ src/components/notes (backups)/App-class-component-initial-render.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react"
+// equivalent to import {default as React Component } from "react"
+
+class App extends Component {
+  render(props) {
+    return <div></div>
+  }
+}
+
+// equivalent to:
+// export default class App extends Component {
+
+export default App
---
+++ src/components/notes (backups)/App-functional-component-initial-render.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react"
+// equivalent to import { default as React, Component } from "react"
+
+// {Component} is unused with functional component
+
+const App = (props) => <div></div>
+
+export default App
+
+// equivalent to export default App = (props) => <div></div>
+
+// equivalent to the class component
