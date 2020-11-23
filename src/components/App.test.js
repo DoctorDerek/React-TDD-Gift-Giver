01e27b331eec7368b5ca292d6ff054f535aaa3e1
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2020-11-23 14:04:03 -0500

    Added a test

--- src/components/App.test.js
+++ src/components/App.test.js
@@ -14,6 +14,8 @@ it("Initializes `state` as an empty array of gifts", () => {
 })
 
 describe("when clicking the `Add Gift` button", () => {
+  const id = 1
+
   beforeEach(() => {
     app.find("#add-gift-button").simulate("click")
   })
@@ -23,10 +25,25 @@ describe("when clicking the `Add Gift` button", () => {
   })
 
   it("Adds a gift to `state` when the `Add Gift` button is clicked", () => {
-    expect(app.state()).toEqual({ gifts: [{ id: 1 }] })
+    expect(app.state()).toEqual({ gifts: [{ id }] })
   })
 
   it("Adds a new gift to the rendered list", () => {
     expect(app.find("#gift-list").children().length).toEqual(1)
+  })
+
+  it("Creates a gift component", () => {
+    expect(app.find("Gift").exists()).toBe(true)
+  })
+
+  describe("and the user wants to remove a gift", () => {
+    beforeEach(() => {
+      app.instance().removeGift(id)
+    })
+    it("Removes the gift with that id from `state`", () => {
+      // app.find("remove-gift").simulate("click")
+      // @ts-ignore because we don't know what's in state
+      expect(app.state().gifts).toEqual([])
+    })
   })
 })
